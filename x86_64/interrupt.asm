5f82d6ff3ba7c1a35ebbefe0742349ef0e91fae0
Gyömörey Domonkos <domonkosgyomorey@gmail.com>
2023-08-04 18:25:07 +0200

    interrupt assembly code refactoring

--- x86_64/interrupt.asm
+++ x86_64/interrupt.asm
@@ -1,20 +1,17 @@
-; Defined in isr.c
 [extern isr_handler]
 [extern irq_handler]
 
 ; Common ISR code
 isr_common_stub:
-    ; 1. Save CPU state
-	pusha ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
-	mov ax, ds ; Lower 16-bits of eax = ds.
-	push eax ; save the data segment descriptor
-	mov ax, 0x10  ; kernel data segment descriptor
+	pusha
+	mov ax, ds                  ; Lower 16-bits of eax = ds.
+	push eax                    ; save the data segment descriptor
+	mov ax, 0x10                ; kernel data segment descriptor
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 	
-    ; 2. Call C handler
 	call isr_handler
 	
     ; 3. Restore state
@@ -24,9 +21,9 @@ isr_common_stub:
 	mov fs, ax
 	mov gs, ax
 	popa
-	add esp, 8 ; Cleans up the pushed error code and pushed ISR number
+	add esp, 8                  ; Cleans up the pushed error code and pushed ISR number
 	sti
-	iret ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
+	iret                        ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
 
 ; Common IRQ code. Identical to ISR code except for the 'call' 
 ; and the 'pop ebx'
@@ -39,8 +36,8 @@ irq_common_stub:
     mov es, ax
     mov fs, ax
     mov gs, ax
-    call irq_handler ; Different than the ISR code
-    pop ebx  ; Different than the ISR code
+    call irq_handler            ; Different than the ISR code
+    pop ebx                     ; Different than the ISR code
     mov ds, bx
     mov es, bx
     mov fs, bx
@@ -50,14 +47,6 @@ irq_common_stub:
     sti
     iret 
 	
-; We don't get information about which interrupt was caller
-; when the handler is run, so we will need to have a different handler
-; for every interrupt.
-; Furthermore, some interrupts push an error code onto the stack but others
-; don't, so we will push a dummy error code for those which don't, so that
-; we have a consistent stack for all of them.
-
-; First make the ISRs global
 global isr0
 global isr1
 global isr2
